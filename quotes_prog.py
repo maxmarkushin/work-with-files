55b6b2d9506ea19832089f0b6b00f35b80e1532e
maxmarkushin <79195298+maxmarkushin@users.noreply.github.com>
2021-02-24 20:02:16 +0300

    Add files via upload

--- quotes_prog.py
+++ quotes_prog.py
@@ -4,10 +4,20 @@ with open("quotes.txt","r",encoding= "utf-8") as file:
 print(data)
 with open("quotes.txt","a",encoding= "utf-8") as file:
     a = input("Введите автора")
-    file.write(a)
+    file.write("\n"+a)
 with open("quotes.txt","r",encoding= "utf-8") as file:
     data = file.read()
 print(data)
 
-
-
+zitate = input("Хотите добавить еще одну цитату?  (да/нет)")
+while zitate != "нет":
+    b = input("Введите цитату:")
+    with open("quotes.txt","a",encoding= "utf-8") as file:
+        file.write("\n"+b)
+    c = input("Введите автора:")
+    with open("quotes.txt","a",encoding= "utf-8") as file:
+        file.write("\n"+c)
+    zitate = input("Хотите добавить еще одну цитату?  (да/нет)")
+with open("quotes.txt","r",encoding= "utf-8") as file:
+    data = file.read()
+print(data)
